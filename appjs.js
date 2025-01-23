fafcdff3d01528ce7d083ddf89e2b86221c5aa86
PEDRO MESSIAS <pedromessiasneves23@gmail.com>
2025-01-23 14:54:04 -0300

    MEUS-PROJETOS-EM-PYTHON

--- appjs.js
+++ appjs.js
@@ -6,8 +6,21 @@ const app = express();
 app.use(express.json())
 
 app.get('/',(requisicao,resposta) => {
-    resposta.status(200),send(carros2025);
+    resposta.status(200).send(carros2025);
 });
+
+app.get("/:sigla",(req, res) => {
+    const siglainformada = req.params.sigla.toUpperCase();
+    const carro = carros2025.find(
+        (infoCarro) => infoCarro.sigla === siglainformada
+    );
+    if(!carro){
+        res.status(404).send('não existe carro com essa sigla');
+        return;
+    }
+    res.status(200).send(carro);
+    });
+
 
 app.listen(3001, () => {
     console.log("Servidor rodando na porta 3001");
